c16d0aab7f8e5df8f30e43259a1d26e4364f75e4
Nikita Kuzmin <Nikita_Kuzmin@epam.com>
2022-01-27 13:07:04 +0300

    clean_code_lessons

--- Java/src/main/java/Planes/PassengerPlane.java
+++ Java/src/main/java/Planes/PassengerPlane.java
@@ -2,19 +2,16 @@ package Planes;
 
 import java.util.Objects;
 
-public class PassengerPlane extends Plane{
+public class PassengerPlane extends Plane {
 
-    //=================FIELDS=================
-    private int passengersCapacity;
+    private final int passengersCapacity;
 
-    //=================CONSTRUCTORS=================
-    public PassengerPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, int passengersCapacity) {
+    public PassengerPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity,
+                          int passengersCapacity) {
         super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
         this.passengersCapacity = passengersCapacity;
     }
 
-
-    //=================METHODS=================
     public int getPassengersCapacity() {
         return passengersCapacity;
     }
@@ -22,22 +19,21 @@ public class PassengerPlane extends Plane{
     @Override
     public String toString() {
         return super.toString().replace("}",
-                ", passengersCapacity=" + passengersCapacity +
+            ", passengersCapacity=" + passengersCapacity +
                 '}');
     }
 
-//    @Override
-//    public String toString() {
-//        return super.toString().replace("}",
-//                ", passengersCapacity=" + passengersCapacity +
-//                        '}');
-//    }
-
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof PassengerPlane)) return false;
-        if (!super.equals(o)) return false;
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof PassengerPlane)) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
         PassengerPlane plane = (PassengerPlane) o;
         return passengersCapacity == plane.passengersCapacity;
     }
